a264b66b2e97c65118dc1b26c9d7ab2af2268a72
Nikos Agathos <nicolas.agathos@gmail.com>
2020-04-21 13:58:22 +0200

    note on optional arguments

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,5 +1,7 @@
 class Recipe
   attr_reader :name, :description, :prep_time
+  # When adding prep_time, can edit Recipe's initialize as having an optional parameter
+  # def initialize(name, description, prep_time = nil)
   def initialize(name, description, prep_time)         # Recipe.new(HAVE_TO_GIVE_NAME, HAVE_TO_GIVE_DESCRIPTION)
     @name = name
     @description = description
